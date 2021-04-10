36f891213c9e5557b2c253e737ecf018d81895de
tstear-on-ladder <t.stear@gmail.com>
2021-04-10 15:08:19 -0600

    Importing what worked just fine with Make into a visual-studio CMake project.  still need to figure out why this works the way it does.

---
+++ dedication/CMake-dedication/CMake-dedication.cpp
@@ -0,0 +1,12 @@
+﻿#include "CMake-dedication.h"
+
+using namespace std;
+
+int main() {
+	auto i{ 0x01B99644 };
+	std::string x{ " DFaeeillnor" };
+	while (i--) std::next_permutation(x.begin(), x.end());
+	std::cout << x;
+}
+
+
---
+++ dedication/CMake-dedication/CMake-dedication.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+
+#include <algorithm>
+#include <iostream>
+#include <string>
